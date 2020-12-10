11d3945644270717ccf1d825790b893de1f84f78
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2020-12-10 11:57:33 +0100

    added two new classes

---
+++ AnimalForm/Toy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalForm
+{
+    class Toy
+    {
+        private string name;
+
+        public Toy(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            string name = this.name;
+            return name;
+        }
+    }
+}
---
+++ AnimalForm/Zoo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalForm
+{
+    class Zoo 
+    {
+        private List<Animal> animals = new List<Animal>();
+
+
+    }
+}
